a6933e72a91336607793febf43c1606c8dfacd22
krzysko <krzysko@ymail.com>
2022-06-06 16:09:11 +0200

    FindById 01 Text, Edit, Button Views
    + One Activity with one Text, one Edit and two Buttons View

---
+++ app/src/main/java/com/example/bscapp/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.bscapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    //  private lateinit var editTextName2: EditText
+    private lateinit var greeting: TextView
+    private lateinit var editName: EditText
+    private lateinit var countResetTV: TextView
+    private lateinit var countSaveTV: TextView
+
+    private var countReset: Int = 0
+    private var countSave: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        greeting = findViewById<TextView>(R.id.greetingTextView)
+        editName = findViewById<EditText>(R.id.nameEditText)
+
+        countResetTV = findViewById<TextView>(R.id.resetCountTextView)
+        countSaveTV = findViewById<TextView>(R.id.saveCountTextView)
+
+        val saveButton = findViewById<Button>(R.id.saveButton)
+        val resetButton = findViewById<Button>(R.id.resetButton)
+
+        saveButton.setOnClickListener {
+            showGreeting()
+        }
+        resetButton.setOnClickListener {
+            resetGreeting()
+        }
+    }
+    private fun showGreeting() {
+        val greetingWithName = "Elo ${editName.text}"
+        greeting.text = greetingWithName
+        editName.text.clear()
+        countSave++
+        countSaveTV.text = countSave.toString()
+    }
+    private fun resetGreeting() {
+        val defaultGreeting = resources.getString(R.string.hello_unknown)
+        if (greeting.text != defaultGreeting) {
+            countReset++
+            countResetTV.text = countReset.toString()
+        }
+        greeting.text = defaultGreeting
+    }
+}
